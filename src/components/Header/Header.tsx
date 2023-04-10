a5d353745288d9ddf3a099edf688e9c66996bb30
longg20 <longttse140019@fpt.edu.vn>
2023-04-10 11:01:43 +0700

    switch Menu.Item to items

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,13 @@
 import { Menu } from "antd";
 import { StyledHeader, StyledMenu } from "./styles";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { switchBot } from "../../redux/chatbotSlice";
 import { botData } from "../../contexts/botData";
 
 const Header = () => {
     const dispatch = useDispatch();
+    const navigate = useNavigate();
 
     return (
         <StyledHeader className="header">
@@ -15,12 +16,25 @@ const Header = () => {
                 theme="dark"
                 mode="horizontal"
                 defaultSelectedKeys={['chatgpt']}
-                onClick={(e) => dispatch(switchBot(botData.find((bots) => bots.key === e.key)))}
-            >
-                <Menu.Item key="chatgpt"><Link to={"/chatgpt"}>ChatGPT-3.5</Link></Menu.Item>
-                <Menu.Item key="nahidabot"><Link to={"/nahidabot"}>NahidaBot</Link></Menu.Item>
-                <Menu.Item key="tsunbot"><Link to={"/tsunbot"}>TsunBot</Link></Menu.Item>
-            </StyledMenu>
+                onClick={(e) => {
+                    dispatch(switchBot(botData.find((bots) => bots.key === e.key)));
+                    navigate(`/${e.key}`)
+                }}
+                items={[
+                    {
+                        key: 'chatgpt',
+                        label: 'ChatGPT-3.5',
+                    },
+                    {
+                        key: 'nahidabot',
+                        label: 'NahidaBot',
+                    },
+                    {
+                        key: 'tsunbot',
+                        label: 'TsunBot',
+                    },
+                ]}
+            />
         </StyledHeader>
     );
 }
